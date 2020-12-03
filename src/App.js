1d347dea2da0bc63506211242580a8e74118a631
Lee <leemcf4@hotmail.co.uk>
2020-12-03 18:15:24 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
-
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import StoryPage from './pages/StoryPage';
-import Videos from './pages/Videos';
-import WhatsGood from './pages/WhatsGood';
-import WhereToFind from './pages/WhereToFind';
-import Hive from './pages/Hive'
-import CallUs from './pages/CallUs'
-import OurFamily from './pages/OurFamily'
-
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import StoryPage from "./pages/StoryPage";
+import Videos from "./pages/Videos";
+import WhatsGood from "./pages/WhatsGood";
+import WhereToFind from "./pages/WhereToFind";
+import Hive from "./pages/Hive";
+import CallUs from "./pages/CallUs";
+import OurFamily from "./pages/OurFamily";
 
 function App() {
   return (
-
     <BrowserRouter>
-    <div className="App">
-      <header>
-        <Header/>
+      <div className="App">
+        <header>
+          <Header />
+        </header>
 
-      </header>
-          
-      <main>
-
-      
-      <Route path='/ourstory' component={StoryPage}></Route>
-      <Route path='/whatsgood' component={WhatsGood}></Route>
-      <Route path='/ourfamily' component={OurFamily}></Route>
-      <Route path='/findus' component={WhereToFind}></Route>
-      <Route path='/hive' component={Hive}></Route>
-      <Route path='/videos' component={Videos}></Route>
-      <Route path='/callus' component={CallUs}></Route>
-      <Route path='/' component={HomePage} exact ></Route>
-      </main>
-
-        
-      
-    </div>
+        <main>
+          <Route path="/ourstory" component={StoryPage}></Route>
+          <Route path="/whatsgood" component={WhatsGood}></Route>
+          <Route path="/ourfamily" component={OurFamily}></Route>
+          <Route path="/findus" component={WhereToFind}></Route>
+          <Route path="/hive" component={Hive}></Route>
+          <Route path="/videos" component={Videos}></Route>
+          <Route path="/callus" component={CallUs}></Route>
+          <Route path="/" component={HomePage} exact></Route>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/WhereToFind.js
+++ src/pages/WhereToFind.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function WhereToFind() {
-    return (
-        <div>
-            <h1>Find us</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Find us</h1>
+      <div className="yo">
+        asdfasdf
+        <h2></h2>
+      </div>
+    </div>
+  );
 }
 
-export default WhereToFind
+export default WhereToFind;
